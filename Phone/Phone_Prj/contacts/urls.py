4ca5d07e036cfbd5159e15b6d26a60e5aaae6f80
egaeuni <gaeun3619@gmail.com>
2024-05-02 18:46:18 +0900

    Phone

--- Phone/Phone_Prj/contacts/urls.py
+++ Phone/Phone_Prj/contacts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import list, create, detail, update, delete
+
 '''
 urlpatterns = [
     path('', IndexView.as_view(), name = 'index'),
@@ -8,6 +9,13 @@ urlpatterns = [
 '''
 
 urlpatterns = [
-    path('', index, name = 'index'),
-    path('result/', result, name="result"),
+    path('', list, name = 'list'),
+    #path('result/', result, name="result"),
+    path('create/', create, name = "create"),
+    path('detail/<int:id>/', detail, name="detail"),
+    path('update/<int:id>/', update, name="update"),
+    path('delete/<int:id>/', delete, name="delete"),
 ]
+
+
+
--- Phone/Phone_Prj/contacts/views.py
+++ Phone/Phone_Prj/contacts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, RedirectView, CreateView
 from .models import Phone
 
 # Create your views here.
@@ -21,3 +21,49 @@ def result(request):
     search_list = Phone.objects.filter(name__contains = search_keyword).order_by('name')
 
     return render(request, "contacts/result.html", {'result': search_list, 'data':search_keyword})
+
+
+def list(requset):
+    phones = Phone.objects.all().order_by('name')
+    return render(requset,"contacts/list.html", {'phones':phones})
+
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+        
+        phone = Phone.objects.create(
+            name = name,
+            phone_num = phone_num,
+            email = email,
+        )
+        return redirect('list')
+    return render(request, 'contacts/create.html')
+
+
+def detail(request, id):
+    phone = get_object_or_404(Phone, id=id)
+    return render(request, 'contacts/detail.html', {'phone':phone})
+
+def update(request, id):
+    phone = get_object_or_404(Phone, id=id)
+    if request.method == "POST":
+        phone.name = request.POST.get('name')
+        phone.phone_num = request.POST.get('phone_num')
+        phone.email = request.POST.get('email')
+        phone.save()
+        return redirect('detail', id)
+    return render(request, 'contacts/update.html', {'phone':phone})
+
+def delete(request, id):
+    phone = get_object_or_404(Phone, id=id)
+    if request.method == "POST":
+        phone.delete()
+        return redirect('list')
+    return render(request, 'contacts/delete.html', {'phone': phone})
+
+
+
+
